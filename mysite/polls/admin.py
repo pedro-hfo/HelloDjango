57cf43013440c4f6e30db5bf4698cb60c326025d
pedro.hfo <pedro_hfo@hotmail.com>
2022-08-08 00:41:01 +0100

    Updated admin layout

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
-
 # Register your models here.
-from .models import Question
-from .models import Choice
+from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+admin.site.site_header = 'Polls administration'
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
+
